91391182f809d1dd17cb8a766e90fad6e3a20775
syedharishussain <syedharishussain@gmail.com>
2015-02-07 03:50:36 +0500

    added facebook button on login activity

---
+++ app/src/main/java/com/plego/wagerocity/android/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.plego.wagerocity.android.activities;
+
+import android.os.Bundle;
+import com.plego.wagerocity.R;
+
+import roboguice.activity.RoboFragmentActivity;
+
+public class LoginActivity extends RoboFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
--- app/src/main/java/com/plego/wagerocity/android/activities/SplashActivity.java
+++ app/src/main/java/com/plego/wagerocity/android/activities/SplashActivity.java
@@ -1,7 +1,12 @@
 package com.plego.wagerocity.android.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import com.plego.wagerocity.R;
+
+
 
 import roboguice.activity.RoboFragmentActivity;
 
@@ -12,5 +17,12 @@ public class SplashActivity extends RoboFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        }, 1000);
     }
 }

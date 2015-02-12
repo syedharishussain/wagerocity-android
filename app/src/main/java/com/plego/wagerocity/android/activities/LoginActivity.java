5d26383ff1d7f20d31d4f800612b3efde9c0d5e1
syedharishussain <syedharishussain@gmail.com>
2015-02-12 21:22:13 +0500

    Facebook Login working

--- app/src/main/java/com/plego/wagerocity/android/activities/LoginActivity.java
+++ app/src/main/java/com/plego/wagerocity/android/activities/LoginActivity.java
@@ -1,37 +1,81 @@
 package com.plego.wagerocity.android.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
 import com.plego.wagerocity.R;
 import com.facebook.Session;
 import com.plego.wagerocity.utils.AndroidUtils;
 
+import java.util.Arrays;
+
 import roboguice.activity.RoboFragmentActivity;
 
-public class LoginActivity extends RoboFragmentActivity implements Session.StatusCallback {
+public class LoginActivity extends RoboFragmentActivity {
 
     private static final String TAG = "LoginActivity";
+    private UiLifecycleHelper uiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidUtils.printFBKeyHash(this);
         setContentView(R.layout.activity_login);
-
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
     }
 
+//    @Override
+//    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+//        LoginButton authButton = (LoginButton) parent.findViewById(R.id.authButton);
+//        return parent;
+//    }
+
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
 
+        uiHelper.onResume();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
     }
 
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
@@ -42,8 +86,10 @@ public class LoginActivity extends RoboFragmentActivity implements Session.Statu
         }
     }
 
-    @Override
-    public void call(Session session, SessionState state, Exception exception) {
-
-    }
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 }

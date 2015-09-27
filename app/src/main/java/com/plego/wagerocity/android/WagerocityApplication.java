4ad45aeaaf081b458ff8cdd579c1de13bacd3604
HassanJ <hassanjawed17@gmail.com>
2015-09-27 15:52:22 +0500

    Configured Crashlytics to be run only on release build

--- app/src/main/java/com/plego/wagerocity/android/WagerocityApplication.java
+++ app/src/main/java/com/plego/wagerocity/android/WagerocityApplication.java
@@ -3,6 +3,11 @@ package com.plego.wagerocity.android;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
+import com.plego.wagerocity.BuildConfig;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by Hassan Jawed on 9/27/2015.
  */
@@ -15,6 +20,9 @@ public class WagerocityApplication extends Application {
         super.onCreate();
 
         component = DaggerComponentInitializer.initBase(this);
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
     }
 
     public static BaseComponent component(Context context) {
